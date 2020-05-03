11400e8985d756ceddb71d8011bea9d869675efc
Asikur <asikurrahman7@gmail.com>
2020-05-03 16:47:13 +0600

    adding headerMenu component , and navigate homepage, shoppage,contactpage

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import Homepage from "./pages/homepage/Homepage";
 import {Route, Switch} from "react-router-dom";
 import ShopPage from "./pages/ShopPage/ShopPage"
+import HeaderMenu from "./components/headerMenu/HeaderMenu";
 
 
 
 function App() {
   return (
     <div >
+        <HeaderMenu/>
         <Switch>
             <Route exact path='/' component={Homepage}/>
             <Route exact path='/shop' component={ShopPage}/>
--- src/components/menu-item/MenuItem.js
+++ src/components/menu-item/MenuItem.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import './menu-item.style.scss'
-import {withRouter} from "react-router-dom";
+import {Link, withRouter} from "react-router-dom";
 
 function MenuItem({title,imageUrl,size, history,linkUrl,match}) {
     return (
-        <div className={`${size} menu-item`} onClick={()=>history.push(`${match.url}${linkUrl}`)}>
+       <div className={`${size} menu-item`} onClick={()=>history.push(`${match.url}${linkUrl}`)}>
             <div className='background-image'
             style={{backgroundImage:`url(${imageUrl})`}} />
             <div className="content">
